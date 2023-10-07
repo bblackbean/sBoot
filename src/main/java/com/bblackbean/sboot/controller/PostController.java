a052a51e5f553e8a28b3251d5541ae828640a5da
bblackbean <smart13210@gmail.com>
2023-10-07 21:44:31 +0900

    push test3

--- src/main/java/com/bblackbean/sboot/controller/PostController.java
+++ src/main/java/com/bblackbean/sboot/controller/PostController.java
@@ -1,10 +1,25 @@
 package com.bblackbean.sboot.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/post-api")  // 공통된 url 경로
 public class PostController {
+    // http://localhost:8080/api/v1/post-api/member
+    @PostMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
 
 }
